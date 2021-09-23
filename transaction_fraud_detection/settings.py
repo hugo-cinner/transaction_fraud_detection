194bc61f682573564292f9dea62fe56463198c70
Hoang Pham <hoangpham@Hoangs-MacBook-Pro.local>
2021-09-23 16:16:32 +0700

    update setting

--- transaction_fraud_detection/settings.py
+++ transaction_fraud_detection/settings.py
@@ -128,8 +128,8 @@ USE_TZ = True
 # STATIC_URL = 'Media/'
 #
 #
-# MEDIA_ROOT = os.path.join(BASE_DIR, '.')
-# MEDIA_URL = '/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '.')
+MEDIA_URL = '/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
